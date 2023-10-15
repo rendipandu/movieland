9bd57d3912f674da043edc4c99418e0eb075578f
rendi <rendipandu313@gmail.com>
2023-10-15 19:57:11 +0700

    Fix(API): Fixing fetching API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b2fea61";
+const API_URL = "https://www.omdbapi.com?apikey=b2fea61";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
